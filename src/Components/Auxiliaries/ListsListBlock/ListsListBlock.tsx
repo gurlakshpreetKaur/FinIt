731c887dd0978440c40b57052215b9dbf311e783
gurlakshpreetKaur <gurlakshpreetkaur@gmail.com>
2023-05-01 21:22:44 +0530

    improve modal animation system

--- src/Components/Auxiliaries/ListsListBlock/ListsListBlock.tsx
+++ src/Components/Auxiliaries/ListsListBlock/ListsListBlock.tsx
@@ -20,6 +20,7 @@ function ListsListBlock(props: ListsListBlockProps): JSX.Element {
     const listData = useDocumentData(doc(db, "lists", props.id))[0] as listDocument;
 
     const [showSettingsModal, setShowSettingsModal] = useState(false);
+    const [permitShowSettingsModalToExist, setPermitShowSettingsModalToExist] = useState(false);
 
     const [listKeyInput, setListKeyInput] = useState("");
 
@@ -27,38 +28,37 @@ function ListsListBlock(props: ListsListBlockProps): JSX.Element {
         (async () => await updateDoc(doc(db, "lists", props.id), { listKey: listKeyInput }))();
     }, [listKeyInput]);
 
-    // useEffect(() => console.log(showSettingsModal), [showSettingsModal]);
-
     const completedTasks = listData && (listData.tasks ? (Object.keys(listData.tasks).length > 0 ? Object.keys(listData.tasks).map(item => listData.tasks[item]).filter(item => item.finished).length : "-") : "-") as unknown as string;
     const totalTasks = listData && (listData.tasks ? (Object.keys(listData.tasks).length > 0 ? Object.keys(listData.tasks).length : "-") : "-") as unknown as string;
-    const displayTasksCompleted = listData && (completedTasks + " / " + totalTasks)
+    const displayTasksCompleted = listData && (completedTasks + " / " + totalTasks);
+
     return (listData && listData.tasks &&
         <>
-            {showSettingsModal && (<Modal visibiltyHandler={setShowSettingsModal} classNames="settings">
-                <h2>Settings</h2>
-                <br />
-                <p className="flex-div">List Code: <input value={props.id} readOnly title={"List Code: " + props.id} /></p>
-                <br />
-                <p className="flex-div">List Key:
-                    <input value={listKeyInput} onChange={(change) =>
-                        setListKeyInput(change.target.value)
-                    } title={"List Key:" + listKeyInput} /></p>
-                <br />
-
-                <button className="solid-border walnut-brown-border mid-border beaver-bg" onClick={() => {
-                    setBottomText("Copied List Key and List Code!");
-                    console.log(listData.listKey);
-                    if (listData.listKey === "" || listData.listKey === undefined) navigator.clipboard.writeText("Hey! I use a Google Extention called FinIt to manage my to-do lists, here's the list code for my list titled '" + listData.title + "': \n\n List Code: " + props.id + "\n\n Please check it out :)");
-                    else navigator.clipboard.writeText("Hey! I use a Google Extention called FinIt to manage my to-do lists, here's the list code and list key to access my list titled '" + listData.title + "': \n\n List Code: " + props.id + "\n\n List Key: " + listData.listKey + ". \n\n Please check it out :)");
-                }}>Share</button>
-                <button className="solid-border walnut-brown-border mid-border beaver-bg" onClick={(e) => {
-                    e.stopPropagation();
-                    setShowSettingsModal(false);
-                    setBottomText("");
-                }}>Exit</button>
-                <button className="solid-border walnut-brown-border mid-border beaver-bg" title="Delete List">Delete</button>
-                <br /><br />
-            </Modal>)}
+            {permitShowSettingsModalToExist &&
+                <Modal visibiltyHandler={setShowSettingsModal} classNames="settings" isVisible={showSettingsModal} existenceHandler={setPermitShowSettingsModalToExist}>
+                    <h2>Settings</h2>
+                    <br />
+                    <p className="flex-div">List Code: <input value={props.id} readOnly title={"List Code: " + props.id} /></p>
+                    <br />
+                    <p className="flex-div">List Key:
+                        <input value={listKeyInput} onChange={(change) =>
+                            setListKeyInput(change.target.value)
+                        } title={"List Key:" + listKeyInput} placeholder="Optional List Key..." /></p>
+                    <br />
+                    <button className="solid-border walnut-brown-border mid-border beaver-bg" onClick={() => {
+                        setBottomText("Copied List Key and List Code!");
+                        console.log(listData.listKey);
+                        if (listData.listKey === "" || listData.listKey === undefined) navigator.clipboard.writeText("Hey! I use a Google Extention called FinIt to manage my to-do lists, here's the list code for my list titled '" + listData.title + "': \n\n List Code: " + props.id + "\n\n Please check it out :)");
+                        else navigator.clipboard.writeText("Hey! I use a Google Extention called FinIt to manage my to-do lists, here's the list code and list key to access my list titled '" + listData.title + "': \n\n List Code: " + props.id + "\n\n List Key: " + listData.listKey + ". \n\n Please check it out :)");
+                    }}>Share</button>
+                    <button className="solid-border walnut-brown-border mid-border beaver-bg" onClick={(e) => {
+                        e.stopPropagation();
+                        setShowSettingsModal(false);
+                        setBottomText("");
+                    }}>Exit</button>
+                    <button className="solid-border walnut-brown-border mid-border beaver-bg" title="Delete List">Delete</button>
+                    <br /><br />
+                </Modal>}
             <div className="lists-list-block semi-transparent-white-bg rounded" onClick={(e) =>
                 setCurrentPage(["view-list", props.id])
             }>
@@ -66,12 +66,13 @@ function ListsListBlock(props: ListsListBlockProps): JSX.Element {
                     <h2 title={listData.title}>{listData.title}</h2>
                     <span>
                         <button className="small" onClick={(e) => {
-                            e.preventDefault();
                             e.stopPropagation();
-                            if (!showSettingsModal)
+                            if (!permitShowSettingsModalToExist) {
+                                setPermitShowSettingsModalToExist(true);
                                 setShowSettingsModal(true);
+                            }
                             console.log("click");
-                        }}>⚙️</button>
+                        }} title="List Settings">⚙️</button>
                     </span>
                 </div>
                 {/* <br /> */}
--- src/Components/Auxiliaries/Modal/Modal.tsx
+++ src/Components/Auxiliaries/Modal/Modal.tsx
@@ -1,19 +1,34 @@
 import React, { useEffect, useState, useContext, useRef } from "react";
 import "./Modal.css";
 import { BottomContext } from "../../App/App";
+import { isVisible } from "@testing-library/user-event/dist/utils";
 
 interface ModalProps extends React.PropsWithChildren {
+    isVisible: boolean,
+    isExistant?: boolean,
     classNames?: string,
     visibiltyHandler: Function
+    existenceHandler: Function
 }
 function Modal(props: ModalProps) {
     const setBottomText = useContext(BottomContext);
     const elementRef = useRef(null);
+    const [classList, setClassList] = useState(!props.isVisible ? "fade-out" : "");
+    // const [elementToDisplay, setElementToDisplay] = useState(modalEl);
     useEffect(() => {
         return (() => {
             setBottomText("");
         });
     }, []);
+    useEffect(() => {
+        if (!props.isVisible) {
+            setClassList("fade-out");
+            setTimeout(() => {
+                props.existenceHandler(false);
+            }, 1000);
+        }
+        else setClassList("");
+    }, [props.isVisible]);
     window.addEventListener("click", (click) => {
         if (elementRef.current === null) return;
         if (click.target !== elementRef.current && !(elementRef.current as Element).contains(click.target as Node)) {
@@ -21,11 +36,9 @@ function Modal(props: ModalProps) {
         }
         console.log(click.target, !((click.target as Element).classList.contains("modal")));
     }, true);
-    return (
-        <div className={"modal " + props.classNames ?? ""} ref={elementRef}>
-            {props.children}
-        </div>
-    )
+    return <div className={"modal " + (props.classNames || "") + " " + classList} ref={elementRef}>
+        {props.children}
+    </div>;;
 }
 
 export default Modal;
